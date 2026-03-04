a6478097b2ab054a3ccd25911eec2bb9069d9287
Bakaev D. <bakaev@kru.kem>
2026-03-04 10:58:34 +0700

    Add Transaction model

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using PersonalBudget;
+using System.Transactions;
+
+var newTrans = new PersonalBudget.Transaction
+{
+    Id = 1,
+    Amount = 250.50m, // Буква 'm' в конце обязательна для типа decimal!
+    Category = "Еда",
+    Date = DateTime.Now,
+    Description = "Кофе в аэропорту"
+};
+
+Console.WriteLine($"Купил {newTrans.Category} на сумму {newTrans.Amount} руб. ({newTrans.Description})");
---
+++ Transaction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalBudget
+{
+    public class Transaction
+    {
+        public int Id {  get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string Category { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Description { get; set; }
+    }
+}
